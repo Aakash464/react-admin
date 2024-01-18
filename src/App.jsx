101cba5538a95f3ea96c8023d1e6c9d8d3cf24e5
Aakash464 <aakash5678956@gmail.com>
2024-01-18 14:35:28 +0530

    mode

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,21 @@ import{ColorModeContext,useMode} from './theme';
 import { CssBaseline,ThemeProvider } from '@mui/material';
 import TopBar from "./global/TopBar";
 import SideBar from './global/SideBar';
+import { Route,Routes } from 'react-router-dom';
+// import Team from "./scenes/team";
+// import Dashboard from "./scenes/dashboard/index"
+// import Invoices from "./scenes/invoices";
+// import Contacts from "./scenes/contacts";
+// import Bar from "./scenes/bar";
+// import Form from "./scenes/form";
+// import Line from "./scenes/line";
+// import Pie from "./scenes/pie";
+// import FAQ from "./scenes/faq";
+// import Geography from "./scenes/geography";
+// import Calendar from "./scenes/calendar";
+
+
+
 
 
 
@@ -14,9 +29,23 @@ function App() {
     <ThemeProvider theme={theme}>
       <CssBaseline />
         <div className="app">
+          <SideBar />
           <main className="content">
             <TopBar />
-            <SideBar />
+            <Routes>
+              {/* <Route path="/" element={<Dashboard />}></Route> */}
+              {/* <Route path="/team" element={<Team />}></Route> */}
+              {/* <Route path="/contacts" element={<Contacts />}></Route> */}
+              {/* <Route path="/invoices" element={<Invoices />}></Route> */}
+              {/* <Route path="/form" element={<Form />}></Route> */}
+              {/* <Route path="/bar" element={<Bar />}></Route> */}
+              {/* <Route path="/pie" element={<Pie />}></Route> */}
+              {/* <Route path="/line" element={<Line />}></Route> */}
+              {/* <Route path="/faq" element={<FAQ />}></Route> */}
+              {/* <Route path="/geography" element={<Geography />}></Route> */}
+              {/* <Route path="/calendar" element={<Calendar />}></Route> */}
+            </Routes>
+           
           </main>
         </div>
 
--- src/global/SideBar.jsx
+++ src/global/SideBar.jsx
@@ -1,4 +1,8 @@
 import React from 'react';
+import { useState } from 'react';
+import { ProSidebar,Menu,MenuItem } from 'react-pro-sidebar';
+import 'react-pro-sidebar/dist/css/styles.css';
+// import {Box,IconButton,Typography,useTheme} from ''
 
 const SideBar = () => {
     return (
