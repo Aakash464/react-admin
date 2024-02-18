b8011c88cc39abaedb8e24240ae082c46bb33133
Aakash464 <aakash5678956@gmail.com>
2024-02-18 15:52:08 +0530

    sidebar

--- src/global/SideBar.jsx
+++ src/global/SideBar.jsx
@@ -72,7 +72,25 @@ const SideBar = () => {
                         
                         </Box>
                     )}
-                    </MenuItem>    
+                    </MenuItem>   
+                    {/* User  */}
+                    {!isCollapsed && (
+                        <Box mb="25px">
+                            <Box display="flex" justifyContent="center" alignContent="center">
+                                <img alt="profile-user" width="100px" height="100px"
+                                src={`../../public/user.png`}
+                                style={{
+                                    cursor : "pointer",
+                                    borderRadius : "50%"
+                                }}
+                                />
+                            </Box>
+                            <Box textAlign="center">
+                                <Typography variant="h2" color={colors.grey[100]} fontWeight="bold" sx={{m:"10px 0 0 0" }}>Aakash</Typography>
+                                <Typography variant="h5" color={colors.greenAccent[500]}>VP Admin</Typography>
+                            </Box>
+                        </Box>
+                    )}
                 </Menu>
             </ProSidebar>
 
